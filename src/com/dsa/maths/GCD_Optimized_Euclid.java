69650622292a549703cea560e86f96a15050f738
MOHAMMAD-RASHIM <mohammadrashimkallaje@gmail.com>
2026-07-18 10:16:43 +0530

    add optimized euclid algorithm

---
+++ src/com/dsa/maths/GCD_Optimized_Euclid.java
@@ -0,0 +1,37 @@
+package com.dsa.maths;
+
+import java.util.Scanner;
+
+public class GCD_Optimized_Euclid {
+	
+	static int gcd(int a,int b)
+	{
+		while(a!=0 && b!=0)
+		{
+			if(a>b)
+			{
+				a = a%b;
+			}
+			else
+			{
+				b = b%a;
+			}
+		}
+		return a+b;
+		
+	}
+	
+	
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		int result = gcd(a,b);
+		System.out.println(result);
+
+	}
+
+}
