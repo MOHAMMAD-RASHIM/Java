3f8e94be7d1b43fb116968cbf6f8c1dea975fdfa
MOHAMMAD-RASHIM <mohammadrashimkallaje@gmail.com>
2026-07-18 10:09:19 +0530

    wrap logic inside method

--- src/com/dsa/maths/Euclid_GCD.java
+++ src/com/dsa/maths/Euclid_GCD.java
@@ -3,13 +3,9 @@ package com.dsa.maths;
 import java.util.Scanner;
 
 public class Euclid_GCD {
-
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		int a = scanner.nextInt();
-		int b = scanner.nextInt();
-		
+	
+	public static int gcd(int a, int b)
+	{
 		while(a!=b)
 		{
 			if(a>b)
@@ -21,8 +17,17 @@ public class Euclid_GCD {
 				b = b-a;
 			}
 		}
-		System.out.print(a);
-			
+		return a;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		int result = gcd(a,b);
+		System.out.println(result);
 	}
 
 }
