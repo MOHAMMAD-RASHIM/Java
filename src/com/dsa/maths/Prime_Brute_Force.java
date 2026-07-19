1a217b701b078bcd3cfb26f50aabdd38999d8449
MOHAMMAD-RASHIM <mohammadrashimkallaje@gmail.com>
2026-07-19 12:33:52 +0530

    add Prime Number Brute Force

---
+++ src/com/dsa/maths/Prime_Brute_Force.java
@@ -0,0 +1,36 @@
+package com.dsa.maths;
+
+import java.util.Scanner;
+
+public class Prime_Brute_Force {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		long num = scanner.nextLong();
+		
+		if(isPrime(num))
+		{
+			System.out.println(num+" is Prime");
+		}
+		else {
+			System.out.println(num+" is NOT Prime");
+		}
+	}
+
+	private static boolean isPrime(long num) {
+		if(num<=1)
+		{
+			return false;
+		}
+		for(int i=2;i<=num/2;i++)
+		{
+			if(num%i==0)
+			{
+				return false;
+			}
+			
+		}
+		return true;
+	}
+
+}
